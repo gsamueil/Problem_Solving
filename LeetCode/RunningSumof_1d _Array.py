d5b5364d79c08dad2b859929fea106c46a31b865
gsamuel <Gsamuel.it@outlook.com>
2023-02-27 22:29:01 +0200

    write code

--- LeetCode/RunningSumof_1d _Array.py
+++ LeetCode/RunningSumof_1d _Array.py
@@ -1,1 +1,46 @@
 """1480. Running Sum of 1d Array"""
+
+
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+
+"""
+
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []# empty list 
+        c = 0 #counter
+
+        for x in nums:
+            c += x #index +element of list c= c+x  ----> new c= 0+3  and next loop c=3 then new c =3+1=4 and so on 
+            print(c)
+            ans.append(c)
+        return ans
+test = Solution()
+print(test.runningSum([3,1,2,10,1]))
