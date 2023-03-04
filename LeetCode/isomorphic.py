527c00e48059eac8d547a09cc6011d962fdc4976
gsamuel <Gsamuel.it@outlook.com>
2023-03-04 12:00:47 +0200

    move files

--- LeetCode/isomorphic.py
+++ LeetCode/isomorphic.py
@@ -39,7 +39,7 @@ class Solution(object):
         :rtype: bool
         """
         mapST, mapTS = {}, {}
-
+        
         for c1, c2 in zip(s, t):
             if ((c1 in mapST and mapST[c1] != c2) or 
                 (c2 in mapTS and mapTS[c2] != c1)):
