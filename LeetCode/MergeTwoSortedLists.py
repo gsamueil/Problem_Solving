0e95ba341d4e2280fa3d8ec19516160bc87e3415
gsamuel <Gsamuel.it@outlook.com>
2023-04-25 15:19:12 +0200

    solve new problem

---
+++ LeetCode/MergeTwoSortedLists.py
@@ -0,0 +1,44 @@
+"""21. Merge Two Sorted Lists"""
+"""https://leetcode.com/problems/merge-two-sorted-lists/?envType=study-plan&id=level-1"""
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        return dummy.next
+
+
+sol = Solution()
+l1 = [1, 2, 4]
+l2 = [1, 3, 4]
+solv = sol.mergeTwoLists(l1, l2)
+print(solv)
