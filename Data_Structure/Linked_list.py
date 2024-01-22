c339b273f5e11d7bfd256d305a5cb80044606bc3
gsamuel <Gsamuel.it@outlook.com>
2024-01-22 15:47:38 +0200

    linked list

--- Data_Structure/Linked_list.py
+++ Data_Structure/Linked_list.py
@@ -26,12 +26,46 @@ class LinkedList:
                 result+=' -> '
             temp_node=temp_node.next
         return result
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
         
+    def insert(self,index,value):
+        new_node=Node(value)
+        if index <0 or index>self.length:
+            return False
+            
+        elif self.length==0:
+            self.head=new_node
+            self.tail=new_node
+        elif index==0:
+            new_node.next=self.head
+            self.head=new_node
+        else:
+            
+            temp_node=self.head
+            for _ in range(index-1):
+                temp_node=temp_node.next
+            new_node.next=temp_node.next
+            temp_node.next=new_node
+        self.length+=1
+        return True
 
 new_linked_list=LinkedList()
+new_linked_list.insert(0,200)
 new_linked_list.append(5)
 new_linked_list.append(10)
 new_linked_list.append(20)
+
 print(new_linked_list)
+new_linked_list.insert(-5,40000)
+print(new_linked_list)
+
 
 
